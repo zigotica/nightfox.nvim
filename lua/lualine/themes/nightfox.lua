1b9212f20bffeaec7006b880f4fada3ecbc8eb25
zigotica <zigotica@gmail.com>
2021-12-06 16:24:26 +0100

    custom lualine

--- lua/lualine/themes/nightfox.lua
+++ lua/lualine/themes/nightfox.lua
@@ -3,35 +3,39 @@ local colors = require("nightfox.colors").load()
 local nightfox = {}
 
 nightfox.normal = {
-  a = { bg = colors.blue, fg = colors.black },
-  b = { bg = colors.fg_gutter, fg = colors.blue },
-  c = { bg = colors.bg_statusline, fg = colors.fg_sidebar },
+  a = { bg = colors.fg, fg = colors.bg },
+  b = { bg = colors.bg, fg = colors.fg },
+  c = { bg = colors.bg, fg = colors.fg },
 }
 
 nightfox.insert = {
   a = { bg = colors.green, fg = colors.black },
-  b = { bg = colors.fg_gutter, fg = colors.green },
+  b = { bg = colors.green, fg = colors.black },
+  c = { bg = colors.green, fg = colors.black },
 }
 
 nightfox.command = {
-  a = { bg = colors.yellow, fg = colors.black },
-  b = { bg = colors.fg_gutter, fg = colors.yellow },
+  a = { bg = colors.yellow, fg = colors.bg },
+  b = { bg = colors.bg, fg = colors.fg },
+  c = { bg = colors.bg, fg = colors.fg },
 }
 
 nightfox.visual = {
-  a = { bg = colors.magenta, fg = colors.black },
-  b = { bg = colors.fg_gutter, fg = colors.magenta },
+  a = { bg = colors.blue, fg = colors.black },
+  b = { bg = colors.blue, fg = colors.black },
+  c = { bg = colors.blue, fg = colors.black },
 }
 
 nightfox.replace = {
   a = { bg = colors.red, fg = colors.black },
-  b = { bg = colors.fg_gutter, fg = colors.red },
+  b = { bg = colors.red, fg = colors.black },
+  c = { bg = colors.red, fg = colors.black },
 }
 
 nightfox.inactive = {
-  a = { bg = colors.bg_statusline, fg = colors.blue },
-  b = { bg = colors.bg_statusline, fg = colors.fg_gutter, gui = "bold" },
-  c = { bg = colors.bg_statusline, fg = colors.fg_gutter },
+  a = { bg = colors.bg, fg = colors.fg },
+  b = { bg = colors.bg, fg = colors.fg },
+  c = { bg = colors.bg, fg = colors.fg },
 }
 
 return nightfox

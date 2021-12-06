cdb19bc864fc5e5cc455db0a98195b7eb0f18848
zigotica <zigotica@gmail.com>
2021-12-06 15:20:41 +0100

    custom colors for zigotica

--- lua/lightline/colorscheme/nightfox.lua
+++ lua/lightline/colorscheme/nightfox.lua
@@ -3,29 +3,49 @@ local colors = require("nightfox.colors").load()
 local nightfox = {}
 
 nightfox.normal = {
-  left = { { colors.black, colors.blue }, { colors.blue, colors.bg } },
-  middle = { { colors.blue, colors.fg_gutter } },
-  right = { { colors.fg_sidebar, colors.bg_statusline }, { colors.blue, colors.bg } },
+  -- zigotica:
+  left = { { colors.black, colors.fg }, { colors.fg, colors.bg_highlight } },
+  middle = { { colors.black, colors.fg }, { colors.fg, colors.bg_highlight } },
+  right = { { colors.black, colors.fg }, { colors.fg, colors.bg_highlight } },
+  -- left = { { colors.black, colors.blue }, { colors.blue, colors.bg } },
+  -- middle = { { colors.blue, colors.fg_gutter } },
+  -- right = { { colors.fg_sidebar, colors.bg_statusline }, { colors.blue, colors.bg } },
   error = { { colors.black, colors.error } },
   warning = { { colors.black, colors.warning } },
 }
 
 nightfox.insert = {
-  left = { { colors.black, colors.green }, { colors.blue, colors.bg } },
+  -- zigotica:
+  left = { { colors.black, colors.green }, { colors.green, colors.bg_highlight } },
+  middle = { { colors.black, colors.green }, { colors.green, colors.bg_highlight } },
+  right = { { colors.black, colors.green }, { colors.green, colors.bg_highlight } },
+  -- left = { { colors.black, colors.green }, { colors.blue, colors.bg } },
 }
 
 nightfox.visual = {
-  left = { { colors.black, colors.magenta }, { colors.blue, colors.bg } },
+  -- zigotica:
+  left = { { colors.black, colors.blue }, { colors.blue, colors.bg_highlight } },
+  middle = { { colors.black, colors.blue }, { colors.blue, colors.bg_highlight } },
+  right = { { colors.black, colors.blue }, { colors.blue, colors.bg_highlight } },
+  -- left = { { colors.black, colors.magenta }, { colors.blue, colors.bg } },
 }
 
 nightfox.replace = {
-  left = { { colors.black, colors.red }, { colors.blue, colors.bg } },
+  -- zigotica:
+  left = { { colors.black, colors.red }, { colors.red, colors.bg_highlight } },
+  middle = { { colors.black, colors.red }, { colors.red, colors.bg_highlight } },
+  right = { { colors.black, colors.red }, { colors.red, colors.bg_highlight } },
+  -- left = { { colors.black, colors.red }, { colors.blue, colors.bg } },
 }
 
 nightfox.inactive = {
-  left = { { colors.blue, colors.bg_statusline }, { colors.comment, colors.bg } },
-  middle = { { colors.fg_gutter, colors.bg_statusline } },
-  right = { { colors.fg_gutter, colors.bg_statusline }, { colors.comment, colors.bg } },
+  -- left = { { colors.blue, colors.bg_statusline }, { colors.comment, colors.bg } },
+  -- middle = { { colors.fg_gutter, colors.bg_statusline } },
+  -- right = { { colors.fg_gutter, colors.bg_statusline }, { colors.comment, colors.bg } },
+  -- zigotica:
+  left = { { colors.comment, colors.bg } },
+  middle = { { colors.comment, colors.bg } },
+  right = { { colors.comment, colors.bg } },
 }
 
 nightfox.tabline = {
--- lua/nightfox/colors/nordfox.lua
+++ lua/nightfox/colors/nordfox.lua
@@ -14,21 +14,38 @@ function M.init()
     meta       = { name = "nordfox", light = false },
 
     none       = "NONE",
-    bg         = "#2e3440",
+    bg         = "#242220", -- 2e3440
 
-    fg         = "#b9bfca",
-    fg_gutter  = "#4b5668",
+    fg         = "#bdb9b9", -- b9bfca
+    fg_gutter  = "#4c4c47", -- 4b5668
 
     black      = "#3b4252",
-    red        = "#bf616a",
-    green      = "#a3be8c",
-    yellow     = "#ebcb8b",
-    blue       = "#81a1c1",
-    magenta    = "#b48ead",
-    cyan       = "#88c0d0",
+    red        = "#bf5454", -- bf616a
+    green      = "#97b87b", -- a3be8c
+    yellow     = "#e0bb70", -- ebcb8b
+    blue       = "#7496b8", -- 81a1c1
+    magenta    = "#a585e6", -- b48ead
+    cyan       = "#99d7e8", -- 88c0d0
     white      = "#e5e9f0",
-    orange     = "#c9826b",
-    pink       = "#bf88bc",
+    orange     = "#c98b65", -- c9826b
+    pink       = "#cf95c0", -- bf88bc
+
+    -- original colors:
+    -- bg         = "#2e3440",
+
+    -- fg         = "#b9bfca",
+    -- fg_gutter  = "#4b5668",
+
+    -- black      = "#3b4252",
+    -- red        = "#bf616a",
+    -- green      = "#a3be8c",
+    -- yellow     = "#ebcb8b",
+    -- blue       = "#81a1c1",
+    -- magenta    = "#b48ead",
+    -- cyan       = "#88c0d0",
+    -- white      = "#e5e9f0",
+    -- orange     = "#c9826b",
+    -- pink       = "#bf88bc",
 
     -- +15 brightness, +15 saturation
     black_br   = "#465780",
